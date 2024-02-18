cf9d87c7b01d15bc7f1a1dd9106fbe42aa9d67b6
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-18 20:10:05 +0000

    Create ErrorPage component and set to errorElement

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
